8b66f759b1ae22742b74ef614ed8aea59d8377dc
marthamareal <marthamareal@gmail.com>
2019-08-01 22:31:23 +0300

    add login

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('users/', views.UserView.as_view(), name='create_and_list_users'),
+    path('users/login/', views.UserLoginView.as_view(), name='login'),
     path('users/<pk>/', views.SingleUserView.as_view(), name='single_user'),
 
     ]
