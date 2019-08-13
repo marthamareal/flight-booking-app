3ef61b2f03a58eba500c5b94a3e049a10b319b89
marthamareal <marthamareal@gmail.com>
2019-08-13 22:10:07 +0300

    add changes

--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.db import models
 
 from utils.base.model import BaseModel
@@ -15,11 +17,12 @@ class Flight(BaseModel):
     destination = models.CharField(max_length=100, blank=False, null=False)
     departure_time = models.DateTimeField(blank=False, null=False)
     arrival_time = models.DateTimeField(blank=False, null=False)
-    flight_number = models.CharField(max_length=100, unique=True)
+    number = models.CharField(max_length=100, unique=True)
     status = models.CharField(max_length=10, choices=STATUSES)
+    seats = models.ManyToManyField('flight.Seat', related_name='seats')
 
     def __str__(self):
-        return self.flight_number
+        return self.number
 
     @property
     def total_seats(self):
@@ -32,9 +35,5 @@ class Flight(BaseModel):
 
 class Seat(BaseModel):
 
-    flight = models.ForeignKey(Flight, related_name="seats", on_delete=models.CASCADE)
-    seat_number = models.CharField(max_length=10, blank=False, null=False)
-    is_available = models.BooleanField(default=True)
+    seat_number = models.CharField(max_length=10)
 
-    def __str__(self):
-        return f"{self.flight.flight_number}-{self.seat_number}"
--- flight/serializers.py
+++ flight/serializers.py
@@ -1,30 +1,47 @@
+import random
+
 from rest_framework import serializers
 
-from flight.models import Flight
+from flight.models import Flight, Seat
+
+
+class SeatSerializer(serializers.ModelSerializer):
+    """Flight model serializer"""
+    class Meta:
+        model = Seat
+        fields = 'seat_number',
 
 
 class FlightSerializer(serializers.ModelSerializer):
     """Flight model serializer"""
-    flight_number = serializers.SerializerMethodField()
-
+    seats = SeatSerializer(many=True, required=True)
+    number =  serializers.CharField(required=False)
     class Meta:
         model = Flight
-        fields = ('id', 'provider', 'flight_number', 'origin', 'destination',
-                  'arrival_time', 'departure_time', 'status', 'created_at', 'updated_at')
+        fields = ('id', 'provider', 'number', 'origin', 'destination',
+                  'arrival_time', 'departure_time', 'status', 'created_at',
+                  'updated_at', 'seats')
 
         read_only_fields = 'id',
 
-    def get_flight_number(self):
-        return 'KQ-LIV2'
-
     def create(self, data):
-        return Flight.objects.create(**data)
+        seats = data.pop('seats')
+        flight = Flight.objects.create(**data)
+        for seat in seats:
+            try:
+                seat = Seat.objects.get(seat_number=seat.strip())
+                flight.seats.add(seat.id)
+            except Seat.DoesNotExist:
+                flight.seats.create(seat_number=seat.strip())
+
+        return flight
 
     def update(self, instance, validated_data):
         """ Updates a flight from the data provided
         Args:
             instance(object): Flight object updated
-            validated_data(dict): a dictionary of validated data of a flight to update
+            validated_data(dict): a dictionary of validated data of
+                                a flight to update
         Returns:
             flight(object): Flight object updated
         """
