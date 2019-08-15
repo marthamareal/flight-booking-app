42d695fffd75166ece107e6de08f949ca152d1c9
marthamareal <marthamareal@gmail.com>
2019-08-15 04:58:49 +0300

    add cancel booking

--- flight/urls.py
+++ flight/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('flights', views.FlightListView.as_view(), name='list_flights'),
     path('flights/create/', views.FlightCreateView.as_view(), name='create_flights'),
     path('flights/<pk>/', views.SingleFlightView.as_view(), name='single_flight'),
-    path('flights/booking/<pk>/', views.BookFlightView.as_view(), name='book_flight'),
+    path('flights/booking/<uuid:flight>/', views.BookFlightView.as_view(), name='book_flight'),
+    path('flights/booking/<uuid:booking>/cancel/', views.CancelBooking.as_view(), name='cancel-booking'),
 
     ]
 
