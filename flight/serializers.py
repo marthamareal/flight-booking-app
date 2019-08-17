15487c56940443de08dca29fb6da879a40cd1eea
marthamareal <marthamareal@gmail.com>
2019-08-17 03:15:38 +0300

    finish flight booking

--- flight/serializers.py
+++ flight/serializers.py
@@ -60,7 +60,8 @@ class FlightSerializer(serializers.ModelSerializer):
         Returns:
             flight(object): Flight object updated
         """
-        del validated_data['seats']  # Recheck this
+        if validated_data.get('seats'):
+            del validated_data['seats']  # Recheck this
         request = self.context.get('request')
         seats = request.data.get('seats')
         flight = super().update(instance, validated_data)
@@ -93,29 +94,30 @@ class BookingSerializer(serializers.ModelSerializer):
 
     user = serializers.UUIDField(required=True)
     flight = serializers.UUIDField(required=True)
-    seat = SeatSerializer(required=True)
+    seat = SeatSerializer(required=False)
 
     class Meta:
         model = Booking
         fields = ['flight', 'seat', 'user']
 
-    def create(self, validated_data):
-        return Booking.objects.create(**validated_data)
-
-    def validate(self, attrs):
+    def create(self, attrs):
         try:
-            seat = self.context.get('seat')
+            selected_seat = self.context.get('seat')
             user = User.objects.get(pk=attrs.get('user'))
             flight = Flight.objects.get(pk=attrs.get("flight"))
-            seat = flight.seats.get(seat_number=seat)
-            if Booking.objects.filter(flight=flight.id, seat=seat.id):
+            if selected_seat:
+                seat = flight.seats.get(seat_number=selected_seat)
+            else:
+                seat = self.auto_select_seat(flight)
+            if Booking.objects.filter(flight=flight.id, seat=seat.id, status='active'):
                 raise serializers.ValidationError(
                     "This seat is already taken")
-            return {
+            validated_data = {
                 "user": user,
                 "flight": flight,
                 "seat": seat
             }
+            return Booking.objects.create(**validated_data)
         except Flight.DoesNotExist:
             raise ValidationError(
                 "Flight does not exist")
@@ -124,5 +126,30 @@ class BookingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "Seat does not exist")
 
-    def update(self, instance, validated_data):
-        return super().update(instance, **validated_data)
+    def update(self, instance, attrs):
+        try:
+            user = User.objects.get(pk=attrs.get('user'))
+            flight = Flight.objects.get(pk=attrs.get("flight"))
+            validated_data = {
+                "user": user,
+                "flight": flight,
+                "status": 'closed'
+            }
+            return super().update(instance, validated_data)
+        except Flight.DoesNotExist:
+            raise ValidationError(
+                "Flight does not exist")
+        except Seat.DoesNotExist:
+            raise serializers.ValidationError(
+                "Seat does not exist")
+
+    def auto_select_seat(self, flight):
+        booked = list(flight.bookings.filter(status='active').values_list('seat_id', flat=True))
+        flight_seats = list(flight.seats.all().values_list('id', flat=True))
+        available_seats = []
+        for seat in flight_seats:
+            if seat not in booked:
+                available_seats.append(seat)
+        if available_seats:
+            return Seat.objects.get(id=random.choices(available_seats)[0])
+        raise ValidationError("Sorry, There are no available seats on this Flight")
